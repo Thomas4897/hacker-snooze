036aa1df124cb6ddec5b92d09905797151a2db7e
Thomas4897 <t.maynard4897@gmail.com>
2021-11-25 14:18:51 +0800

    Adding CSS

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const body = $("body");
 const content = $("#content");
+const displayCommentsBtn = document.querySelector("#displayCommentsBtn");
 
 const HNBaseUrl = "https://hacker-news.firebaseio.com/v0/";
 
@@ -23,25 +24,34 @@ async function makeHttpRequest(url) {
 
 async function getHNTopStoriesData() {
 	const data = await makeHttpRequest(HNTopStories);
+	console.log("start");
 
-	for (id of data) {
-		const idURL = `${HNBaseUrl}/item/${id}.json?print=pretty`;
+	for (let i = 0; i < 100; i++) {
+		const idURL = `${HNBaseUrl}/item/${data[i]}.json?print=pretty`;
 		const idData = await makeHttpRequest(idURL);
-
+		console.log(i);
 		const newDiv = $(`
-        <div id="${id}">
+        <div id="${data[i]}" class="topStories">
             <a href="${idData.url}">
                 ${idData.title}
             </a>
             <div id="idInfo"> 
             Score: ${idData.score}  | 
             Comments: ${idData.descendants} | 
-            Author: ${idData.by} 
+            Author: ${idData.by}  
             </div>
+			<button id="displayCommentsBtn" type="button" class="btn btn-secondary">Display Comments</button>
         </div>`);
 
 		content.append(newDiv);
 	}
+
+	displayCommentsBtn.addEventListener("click", function () {
+		console.log("Working");
+		// alert("It works!");
+	});
+
+	console.log("finished");
 }
 
 getHNTopStoriesData();
